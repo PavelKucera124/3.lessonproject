edad661216314f73b9c8b3ca75c88da62fee50a8
PavelKucera124 <pkucera124@gmail.com>
2024-11-13 14:24:33 +0100

    Initial commit

---
+++ 3.Lesson_notes.R
@@ -0,0 +1,9 @@
+#Version control
+#Helps keeping track of changes
+#Git keeps check of changes in files 
+#Rcript record of code
+#R is self containdep proroject, paer containing R sripts, data, ifgures
+install.packages("usethis")
+library("usethis")
+usethis::use_git()
+1
